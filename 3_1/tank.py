ed9fe0e01f2a5d7b7185eba47e14b568929ea215
marisamak <marinasamakova1507@gmail.com>
2024-10-06 15:56:36 +0300

    add dir 2_1

---
+++ 3_1/tank.py
@@ -0,0 +1,69 @@
+
+# 2 задача размесить танки на канвасе,
+# добавить переменные класса (количество созданных танков, размер танка)
+class Tank:
+    count = 0 # общее количество изготовленных танков
+    SIZE = 100  # размер танка
+    def __init__(self, canvas, x, y,model = 'Т-14 Армата', ammo = 100, speed = 10):
+        self.canvas = canvas
+        Tank.count += 1
+        self.model = model
+        self.hp = 100
+        self.xp = 0
+        self.ammo = ammo
+        self.fuel = 100
+        self.speed = speed
+        self.x = x
+        self.y = y
+        if self.x < 0:
+            self.x = 0
+        if self.y < 0:
+            self.y = 0
+
+        self.create()  # вызов функции отрисовки танка внутри конструктора
+
+    # Методы класса
+
+    def fire(self): # метод для стрельбы и подсчета боеприпасов
+        if self.ammo > 0:
+            self.ammo -= 1
+            print('стреляю')
+
+    def forvard(self):  # по оси y вверх
+        if self.fuel > 0:
+            self.y += -self.speed
+            self.fuel -= 1
+            self.repaint()  # перерисовка танка
+            print(self)
+
+    def backward(self): # по оси y вниз
+        if self.fuel > 0:
+            self.y += self.speed
+            self.fuel -= 1
+            self.repaint()  # перерисовка танка
+            print(self)
+
+    def left(self):  # по оси x влево
+        if self.fuel > 0:
+            self.x += -self.speed
+            self.fuel -= 1
+            self.repaint()  # перерисовка танка
+            print(self)
+
+    def right(self): # по оси x вправо
+        if self.fuel > 0:
+            self.x += self.speed
+            self.fuel -= 1
+            self.repaint()  # перерисовка танка
+            print(self)
+
+    def create(self): # создание танка на холсте в виде квадратной поверхности через идентификатор
+        self.id = self.canvas.create_rectangle(self.x, self.y, self.x + Tank.SIZE,
+                                               self.y + Tank.SIZE, fill='red')
+
+    def repaint(self):
+        self.canvas.moveto(self.id, x = self.x, y = self.y)
+
+    def __str__(self):
+        return f'координаты: x = {self.x}, y = {self.y}, модель: {self.model}, здоровье: {self.hp}, опыт: {self.xp}, боеприпасы: {self.ammo}'
+
